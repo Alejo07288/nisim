4aaf02f82e07e9682965e3d3e28d9982983899ba
Alejo07288 <128153448+Alejo07288@users.noreply.github.com>
2023-05-31 12:27:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>La vida de Nisim</title>
+<style>
+        body {
+                background-image: url("ruta/a/foto-de-fondo.jpg");
+                background-size: cover;
+                color: white;
+                font-family: Arial, sans-serif;
+                }
+
+                h1 {
+                text-align: center;
+                }
+
+                .content {
+                max-width: 800px;
+                margin: 0 auto;
+                padding: 20px;
+                }
+
+                .section {
+                margin-bottom: 40px;
+                }
+
+                .section-title {
+                font-size: 24px;
+                margin-bottom: 10px;
+                }
+
+                .section-content {
+                font-size: 16px;
+                }
+
+                .comments {
+                margin-top: 40px;
+                padding-top: 20px;
+                border-top: 1px solid white;
+                }
+
+                .comment {
+                margin-bottom: 20px;
+                }
+
+                .comment-author {
+                font-weight: bold;
+                }
+
+                .comment-text {
+                margin-top: 5px;
+                }
+</style>
+</head>
+<body>
+<div class="content">
+<h1>La vida de Nisim</h1>
+
+<div class="section">
+<h2 class="section-title">Competencia en triatlón</h2>
+<p class="section-content">Aquí puedes encontrar fotos y relatos de mis competencias en el triatlón. ¡Explora mis emocionantes aventuras y descubre el mundo de este desafiante deporte!</p>
+</div>
+
+<div class="section">
+<h2 class="section-title">Entrenamiento de fuerza y resistencia</h2>
+<p class="section-content">Descubre los secretos detrás de mis entrenamientos en el triatlón. Compartiré mis rutinas de fuerza y resistencia en los tres deportes: natación, ciclismo y carrera a pie.</p>
+</div>
+
+<div class="section">
+<h2 class="section-title">Comentarios</h2>
+<div class="comments">
+<div class="comment">
+<p class="comment-author">Usuario1</p>
+<p class="comment-text">¡Fascinante! Me encanta el triatlón y siempre he querido conocer más sobre los entrenamientos. ¡Espero con ansias tus actualizaciones!</p>
+</div>
+<div class="comment">
+<p class="comment-author">Usuario2</p>
+<p class="comment-text">Increíbles fotos. Parece que estás viviendo una vida emocionante. ¿Alguna recomendación para principiantes en el triatlón?</p>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
